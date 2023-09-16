118c58853d55ce1e7f3d12eb6e3dceb6c23aa6df
Michael Heider <miheider@c3r8p3.42vienna.com>
2023-09-16 13:47:47 +0200

    update

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -15,7 +15,7 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int	i;
-	if (n == 0)
+	if ((int)n == 0)
 		return (0);	
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && i < (int)n)
