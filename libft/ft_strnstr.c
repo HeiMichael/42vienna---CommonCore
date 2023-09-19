adbf82de10437b6c7d86ef1a286410b501ecf048
HeiMichael <145189602+HeiMichael@users.noreply.github.com>
2023-09-19 11:02:41 +0000

    update

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -12,48 +12,45 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char    *ft_strnstr(const char *big, const char *little, size_t len)
 {
-	char	*string;
-	char	*small;
-	size_t	i;
-	size_t	a;
+    char    *string;
+    char    *small;
+    size_t    i;
+    size_t    a;
 
-	string = (char *)big;
-	small = (char *)little;
-	if (*small == '\0')
-		return (string);
-	a = ft_strlen(small);
-	if (ft_strlen(string) < a || len <= 0 || a == 0)
-		return (NULL);
-	i = 0;
-	while ((i <= (len - a)) && string[i] != '\0')
-	{
-		if (string[i] == small[0])
-		{
-			string = &string[i];
-			if (ft_memcmp(string, small, a) == 0)
-				return (string);
-			else
-				return (NULL);
-		}
-		i++;
-	}
-	return (0);
+    string = (char *)big;
+    small = (char *)little;
+    if (*small == '\0')
+        return (string);
+    a = ft_strlen(small);
+    if (ft_strlen(string) < a || len <= 0 || a == 0)
+        return (NULL);
+    i = 0;
+    while (((i + a) <= len) && string[i] != '\0')
+    {    
+        if (string[i] == small[0])
+        {
+          string = &string[i];
+          if (ft_strncmp(string, small, a) == 0)  
+            return (string);
+          else if (ft_strncmp(string, small, a) != 0)
+            i++;
+        }
+        else
+          i++;
+    }
+    return (0);
 }
-
 /*
-#include <bsd/string.h>
-#include <stdio.h>
-#include <string.h>
 int main(void)
 {
-	char big[] = "Jacobo steht der Pizza am 12er!";
-	char little[] = "steht der";
-	int	len = 13;
+    char big[] = "aaabcabcd";
+    char little[] = "abcd";
+    int    len = 9;
 
-	printf("%s", ft_strnstr(big, little, len));
-	printf("\n%s", strnstr(big, little, len));
-	return (0);
+    printf("%s", ft_strnstr(big, little, len));
+//    printf("\n%s", strnstr(big, little, len));
+    return (0);
 }*/
 
