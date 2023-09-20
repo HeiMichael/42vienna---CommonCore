799ce12d6bba04883b917d85210ef8eac8dc8bf5
HeiMichael <145189602+HeiMichael@users.noreply.github.com>
2023-09-20 21:04:26 +0000

    update

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -89,3 +89,19 @@ char	**ft_split(char const *s, char c)
 	table = setarray(s, table, c);
 	return (table);
 }
+
+int	main(void)
+{
+	char	**result = ft_split("tripouille", 0);
+	int i = 0;
+    while (i < 3)
+		printf("{%s} ", result[i++]);
+	// while (result[i])
+		// printf("{%s} ", result[i++]);
+	printf("\n");
+	i = 0;
+	while (result[i])
+		free(result[i++]);
+    free(result);
+	return (0);
+}
