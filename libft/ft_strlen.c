442c770bce53b8041e8a23e87d53891188655996
HeiMichael <145189602+HeiMichael@users.noreply.github.com>
2023-09-23 14:37:20 +0000

    update

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -17,7 +17,16 @@ size_t	ft_strlen(const char *s)
 	unsigned int	a;
 
 	a = 0;
+	if (!s && !*s)
+		return (0);
 	while (s[a] != '\0')
 		a++;
 	return (a);
 }
+/*
+int main(void)
+{
+	char	a[];
+	
+	printf("%d", "What is going on!");
+}*/
