83dfb00995983671db263b05d58a75ab0585e8b1
Michael Heider <145189602+HeiMichael@users.noreply.github.com>
2023-11-19 18:12:28 +0100

    "update mandelbrot"

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: miheider <miheider@42>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 13:23:48 by miheider          #+#    #+#             */
-/*   Updated: 2023/11/19 16:09:38 by miheider         ###   ########.fr       */
+/*   Updated: 2023/11/19 18:11:24 by miheider         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,20 @@
 #include <X11/keysym.h>
 #include <math.h>
 
-#define WIDTH		600
-#define HEIGHT		600
-#define ITER_MAX	70
-#define COLOR1		0x000000
-#define COLOR2		0x123456
-
+#define WIDTH		800
+#define HEIGHT		800
+#define ITER_MAX	40
+#define COLOR1		0x0
+#define ESCAPE		40
+#define GREEN_MIN	20
+#define GREEN_MAX	255
+#define GREEN_SHIFT	25
+#define RED_MIN		1
+#define RED_MAX		1
+#define RED_SHIFT	1
+#define BLUE_MIN	1
+#define BLUE_MAX	1
+#define BLUE_SHIFT	1
 
 typedef struct	s_data
 {
@@ -69,5 +77,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strdup(const char *s);
 void	julia(double x, double y, t_fractol *red_thread);
 int		ft_atoi(const char *nptr);
+double	check_for_color (t_fractol *red_thread);
 
 #endif
--- mandelbrot.c
+++ mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: miheider <miheider@42>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 19:44:35 by miheider          #+#    #+#             */
-/*   Updated: 2023/11/19 16:21:25 by miheider         ###   ########.fr       */
+/*   Updated: 2023/11/19 17:55:09 by miheider         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,29 @@
 // - Divergenz(weiß/bunt) oder Konvergenz(schwarz)
 
 #include "fractol.h"
-#include "stdio.h"
+
+double	check_for_color (t_fractol *red_thread)
+{
+	double	log_temp;
+	double	i;
+	double	iterations;
+
+	iterations = 0.0;
+	log_temp = log(red_thread->z_real * red_thread->z_real + red_thread->z_im * red_thread->z_im) / 2;
+	i = log(log_temp / log(2)) / log(2);
+	iterations = red_thread->iter + 1 - i;
+	return (iterations);
+}
+
 
 void	mandelbrot(int x, int y, double re, double im, t_fractol *red_thread)
 {
+	double	iterations;
+	int	color;
+	int	red;
+	int	green;
+	int	blue;
+
 	red_thread->z_real = 0.0;
 	red_thread->z_im = 0.0;
 	red_thread->iter = 0;
@@ -32,12 +51,25 @@ void	mandelbrot(int x, int y, double re, double im, t_fractol *red_thread)
 		red_thread->z_im = 2 * red_thread->temp * red_thread->z_im + 	//Imag comp
 			red_thread->c_im;
 		if (fabs(red_thread->z_real * red_thread->z_real +
-			red_thread->z_im * red_thread->z_im) > 2147483647)
+			red_thread->z_im * red_thread->z_im) > ESCAPE * ESCAPE)
 			break ;
 		red_thread->iter++;
 	}
+	iterations = check_for_color(red_thread);
 	if (red_thread->iter == ITER_MAX)
 		my_mlx_pixel_put(&red_thread->image, x, y, COLOR1);
 	else
-		my_mlx_pixel_put(&red_thread->image, x, y, COLOR2 * red_thread->iter);
+	{
+		red = (int)(((int)fmod(iterations * RED_SHIFT, (double)(RED_MAX - RED_MIN))) + RED_MIN);
+		green = (int)(((int)fmod(iterations * GREEN_SHIFT, (double)(GREEN_MAX - GREEN_MIN))) + GREEN_MIN);
+		blue = (int)(((int)fmod(iterations * BLUE_SHIFT, (double)(BLUE_MAX - BLUE_MIN))) + BLUE_MIN);
+		color = (red << 16) | (green << 8) | blue;
+		my_mlx_pixel_put(&red_thread->image, x, y, color);
+	}
+
+
+//	if (red_thread->iter == ITER_MAX)
+//		my_mlx_pixel_put(&red_thread->image, x, y, COLOR1);
+//	else
+//		my_mlx_pixel_put(&red_thread->image, x, y, COLOR2 * red_thread->iter);
 }
