78a5f42c8e49e985e6730c9c2bdd897d93c24844
Michael Heider <145189602+HeiMichael@users.noreply.github.com>
2024-11-22 18:20:45 +0100

    Add files via upload

---
+++ ex02/includes/PmergeMe.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miheider <miheider@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 15:20:05 by miheider          #+#    #+#             */
+/*   Updated: 2024/11/22 13:54:05 by miheider         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <deque>
+#include <list>
+#include <cstring>
+#include <cstdlib>
+#include <utility>
+#include <ctime>
+#include <algorithm>
+#include <iomanip>
+
+class PmergeMe {
+    private:
+        int         					_struggler_deque;
+        int                             _size_input;
+        std::deque<int>					_input_deque;
+        std::deque<std::deque<int> >	_helper_deque;
+		std::deque<int>		            _size_deque;
+        std::deque<int>                 _jacobsthal_deque;
+        std::deque<int>                 _length;
+
+		void							pair_deque();
+		void							sort_deque_first();
+		void							sort_back_deque();
+        void                            create_jacobsthal_deque(int n);
+        void                            is_sorted_deque(int argc);
+    public:
+        PmergeMe();
+        PmergeMe(int argc, char **argv);
+        ~PmergeMe();
+        PmergeMe(const PmergeMe& other);
+        PmergeMe& operator=(const PmergeMe& other);
+
+        void    sort_deque(std::deque<int>& input);
+};
